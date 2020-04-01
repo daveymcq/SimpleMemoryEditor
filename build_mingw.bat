23b440dadc378e82048389faa71e54fc0c2f12c7
David McHugh <mcqman1@gmail.com>
2020-04-01 00:01:48 -0400

    Modify build script.

--- build_mingw.bat
+++ build_mingw.bat
@@ -1,8 +1,10 @@
 @echo off
-mkdir bin\x86>NUL
-mkdir bin\x64>NUL
+
+mkdir bin\x86
+mkdir bin\x64
+
 windres ".\data\resource.rc" --target=pe-i386 -o ".\data\res32.o"
 windres ".\data\resource.rc" --target=pe-x86-64 -o ".\data\res64.o"
-gcc ".\src\main.c" ".\data\res32.o" -o ".\bin\x86\Simple Memory Editor (32-bit).exe" -lcomctl32 -lgdi32 -mwindows -m32 -static
-gcc ".\src\main.c" ".\data\res64.o" -o ".\bin\x64\Simple Memory Editor (64-bit).exe" -lcomctl32 -lgdi32 -mwindows -m64 -static
-echo Done!
+
+g++ ".\src\main.cpp" ".\data\res32.o" -o ".\bin\x86\Simple Memory Editor (32-bit).exe" -lcomctl32 -lgdi32 -mwindows -m32 -static
+g++ ".\src\main.cpp" ".\data\res64.o" -o ".\bin\x64\Simple Memory Editor (64-bit).exe" -lcomctl32 -lgdi32 -mwindows -m64 -static
--- build_vs.bat
+++ build_vs.bat
@@ -1,18 +1,18 @@
 @echo off
 
-mkdir bin\x86>NUL
-mkdir bin\x64>NUL
+mkdir bin\x86
+mkdir bin\x64
 
-@%VC64%
+call %VC64%
 rc ".\data\resource.rc"
-cl ".\src\main.c" ".\data\resource.res" /MT /link /subsystem:windows,5.02 /out:".\bin\x64\Simple Memory Editor (64-bit).exe"
+cl ".\src\main.cpp" ".\data\resource.res" /MT /link /subsystem:windows,5.02 /out:".\bin\x64\Simple Memory Editor (64-bit).exe"
 
-@%VC32%
+call %VC32%
 rc ".\data\resource.rc"
-cl ".\src\main.c" ".\data\resource.res" /MT /link /subsystem:windows,5.02 /out:".\bin\x86\Simple Memory Editor (32-bit).exe"
+cl ".\src\main.cpp" ".\data\resource.res" /MT /link /subsystem:windows,5.02 /out:".\bin\x86\Simple Memory Editor (32-bit).exe"
 
 del ".\*.obj"
 del ".\bin\x64\*.manifest"
 del ".\bin\x86\*.manifest"
 
-echo Done!
+pause
