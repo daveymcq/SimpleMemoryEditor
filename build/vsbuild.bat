4dbac8e09154bed16bfa3e4b159c5fc24b2af6b6
David McHugh <mcqman1@gmail.com>
2021-01-11 09:30:00 -0500

    Modify build script.

--- build/vsbuild.bat
+++ build/vsbuild.bat
@@ -1,27 +1,36 @@
 @echo off
 
-echo Compiling for x86...
-echo .
+if [%1] == [] (
 
-set VCVARS=%1\vcvarsall.bat
+    echo Error: Provide the directory that contains your Visual Studio installation's "vcvarsall.bat" as a command line argument.
+  
+) else (
 
-call %VCVARS% x86>NUL
+    echo .....................................................................................................
+    echo Compiling for x86
+    echo .....................................................................................................
+    
+    call %1\vcvarsall.bat x86>NUL
 
-mkdir "..\bin\x86" 2>NUL
-rc "..\resources\icon.rc"
-cl "..\main.c*" "..\resources\icon.res" /MT /link /entry:mainCRTStartup /nodefaultlib:vcruntime.lib  /subsystem:windows,4.02 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe"
-del "..\bin\x86\*.manifest" 2>NUL
-del "..\resources\*.res" 2>NUL
-del "*.obj" 2>NUL
+    mkdir "..\bin\x86" 2>NUL
+    rc "..\resources\icon.rc"
+    cl "..\main.c*" "..\resources\icon.res" /MT /link /nodefaultlib:vcruntime.lib /entry:mainCRTStartup /subsystem:windows,4.02 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe"
+    del "..\bin\x86\*.manifest" 2>NUL
+    del "..\resources\*.res" 2>NUL
+    del "*.obj" 2>NUL
 
-echo .
-echo Compiling for x64...
+    echo .....................................................................................................
+    echo Compiling for x64
+    echo .....................................................................................................
+    
+    call %1\vcvarsall.bat x64>NUL
 
-call %VCVARS% x64>NUL
-
-mkdir "..\bin\x64" 2>NUL
-rc "..\resources\icon.rc"
-cl "..\main.c*" "..\resources\icon.res" /MT /link /entry:mainCRTStartup /nodefaultlib:vcruntime.lib  /subsystem:windows,5.02 /out:"..\bin\x64\Simple Memory Editor (64-bit).exe"
-del "..\bin\x64\*.manifest" 2>NUL
-del "..\resources\*.res" 2>NUL
-del "*.obj" 2>NUL
+    mkdir "..\bin\x64" 2>NUL
+    rc "..\resources\icon.rc"
+    cl "..\main.c*" "..\resources\icon.res" /MT /link /nodefaultlib:vcruntime.lib /entry:mainCRTStartup /subsystem:windows,5.02 /out:"..\bin\x64\Simple Memory Editor (64-bit).exe"
+    del "..\bin\x64\*.manifest" 2>NUL
+    del "..\resources\*.res" 2>NUL
+    del "*.obj" 2>NUL
+    
+    echo .....................................................................................................
+)
