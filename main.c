796b57572103b1da240e94906ea540ef90f1d80d
David McHugh <mcqman1@gmail.com>
2021-08-11 07:44:23 -0400

    Update last modified date.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* Memory Editing Application
  Author: David McHugh Jr.
- Last Modified: 07/15/2021 */
+ Last Modified: 08/11/2021 */
 
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
