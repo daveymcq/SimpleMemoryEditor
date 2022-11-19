ec06ab033e3cae060198d20c022d31abb2a5f3cb
David McHugh <mcqman1@gmail.com>
2022-11-19 02:41:32 -0500

    restructure project

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/* Memory Editing Application
+ Author: David McHugh Jr.
+ Last Modified: 05/22/2022 */
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#define WIN32_LEAN_AND_MEAN
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+
+#include <windows.h>
+#include <tlhelp32.h>
+#include <commctrl.h>
+
+#include "initialize.h"
+
+/* Entry point. */
+
+int32 main(int32 argc, string argv[])
+{
+    if(Initialize(GetModuleHandle(null)))
+    {
+        if(CreateMemoryScannerWindow())
+        {
+            while(GetMemoryScannerWindowMessages());
+        }
+    }
+
+    return 0;
+}
