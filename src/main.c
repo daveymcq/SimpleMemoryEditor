d21010746ec45bb2ac3c13f4191a23ec72640b9e
David McHugh <mcqman1@gmail.com>
2024-09-04 09:17:32 -0400

    Implement an arraylist and modify memory scanner to use an arraylist over a linked list for massive scan time improvements.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /* Memory Editing Application
  Author: David McHugh Jr.
- Last Modified: 03/16/2023 */
+ Last Modified: 09/14/2024 */
 
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
