57cc67bfb90a079c5c536220bdca455006abeca2
David McHugh <mcqman1@gmail.com>
2025-07-22 17:51:33 -0400

    Revert "Fix broken backwards compatibility"
    This reverts commit 13723783c72d4561f3ef70cdc86b807e4abb4d20.

--- build/vs/build.bat
+++ build/vs/build.bat
@@ -19,6 +19,7 @@ if [%1] == [] (
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     cl "..\src\main.c" "..\src\resources\icon.res" /Zl /O1 /MT /link /opt:ref /opt:icf /merge:.rdata=.text /entry:mainCRTStartup /subsystem:windows,5.01 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe" libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -39,6 +40,9 @@ if [%1] == [] (
 =======
     cl "..\src\main.c*" "..\src\resources\icon.res" /Zl /O1 /MT /link libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib /entry:mainCRTStartup /subsystem:windows,5.02 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe"
 >>>>>>> parent of 3b70bb1 (Fix broken backwards compatibility)
+=======
+    cl "..\src\main.c*" "..\src\resources\icon.res" /Zl /O1 /MT /link libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib /entry:mainCRTStartup /subsystem:windows,5.01 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe"
+>>>>>>> parent of 1372378 (Fix broken backwards compatibility)
     del "..\bin\x86\*.manifest" 2>NUL
     del "..\src\resources\*.res" 2>NUL
     del "*.obj" 2>NUL
@@ -51,6 +55,7 @@ if [%1] == [] (
 
     mkdir "..\bin\x64" 2>NUL
     rc "..\src\resources\icon.rc"
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     cl "..\src\main.c" "..\src\resources\icon.res" /Zl /O1 /MT /link /opt:ref /opt:icf /merge:.rdata=.text /entry:mainCRTStartup /subsystem:windows,5.02 /out:"..\bin\x64\Simple Memory Editor (64-bit).exe" libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib 
@@ -77,6 +82,11 @@ if [%1] == [] (
 =======
     del "..\bin\x86\*.manifest" 2>NUL
 >>>>>>> parent of 48ab795 (Fix broken backwards compatibility)
+=======
+    cl "..\src\main.c*" "..\src\resources\icon.res" /Zl /O1 /MT /link libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib /entry:mainCRTStartup /subsystem:windows,5.02 /out:"..\bin\x64\Simple Memory Editor (64-bit).exe"
+    del "..\bin\x64\*.manifest" 2>NUL
+    del "..\src\resources\*.res" 2>NUL
+>>>>>>> parent of 1372378 (Fix broken backwards compatibility)
     del "*.obj" 2>NUL
     
     echo .....................................................................................................
