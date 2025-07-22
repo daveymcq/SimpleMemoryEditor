3785ecce30fae86984f0723cf26ce39444f02944
David McHugh <mcqman1@gmail.com>
2025-07-22 17:31:44 -0400

    Revert "Fix broken backwards compatibility"
    This reverts commit 45d20561f7a447d075e69ec8ebdfcb40a3a65d2f.

--- build/vs/build.bat
+++ build/vs/build.bat
@@ -17,6 +17,9 @@ if [%1] == [] (
     mkdir "..\bin\x86" 2>NUL
     rc "..\src\resources\icon.rc"
     cl "..\src\main.c" "..\src\resources\icon.res" /Zl /O1 /MT /link /opt:ref /opt:icf /merge:.rdata=.text /entry:mainCRTStartup /subsystem:windows,5.01 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe" libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib 
+    cl "..\src\main.c" "..\src\resources\icon.res" /W1 /Oy /Zl /O1 /Oi /MT /GR- /GS- /Gs999999999 /EHa- /fp:precise /fp:except- /nologo /link /incremental:no /stack:0x7fffff,0x7fffff /nodefaultlib /verbose:lib /opt:ref /opt:icf /merge:.rdata=.text /entry:Main /subsystem:windows,5.01 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe" comctl32.lib gdi32.lib user32.lib kernel32.lib 
+    dumpbin /symbols main.obj | findstr aull
+    cl "..\src\main.c" "..\src\resources\icon.res" /Zl /O1 /MT /link /opt:ref /opt:icf /merge:.rdata=.text /entry:mainCRTStartup /subsystem:windows,5.01 /out:"..\bin\x86\Simple Memory Editor (32-bit).exe" libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib 
     del "..\src\resources\*.res" 2>NUL
     del "..\bin\x86\*.manifest" 2>NUL
     del "*.obj" 2>NUL
@@ -29,6 +32,9 @@ if [%1] == [] (
 
     mkdir "..\bin\x64" 2>NUL
     rc "..\src\resources\icon.rc"
+    cl "..\src\main.c" "..\src\resources\icon.res" /Zl /O1 /MT /link /opt:ref /opt:icf /merge:.rdata=.text /entry:mainCRTStartup /subsystem:windows,5.02 /out:"..\bin\x64\Simple Memory Editor (64-bit).exe" libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib 
+    cl "..\src\main.c" "..\src\resources\icon.res" /W1 /Oy /Zl /O1 /MT /Oi /GR- /GS- /Gs999999999 /EHa- /fp:precise /fp:except- /nologo /link /incremental:no /stack:0x7fffff,0x7fffff /nodefaultlib /verbose:lib /opt:ref /opt:icf /merge:.rdata=.text /entry:Main /subsystem:windows,5.02 /out:"..\bin\x64\Simple Memory Editor (64-bit).exe" comctl32.lib gdi32.lib user32.lib kernel32.lib 
+    dumpbin /symbols main.obj | findstr aull
     cl "..\src\main.c" "..\src\resources\icon.res" /Zl /O1 /MT /link /opt:ref /opt:icf /merge:.rdata=.text /entry:mainCRTStartup /subsystem:windows,5.02 /out:"..\bin\x64\Simple Memory Editor (64-bit).exe" libcmt.lib comctl32.lib gdi32.lib user32.lib kernel32.lib 
     del "..\src\resources\*.res" 2>NUL
     del "..\bin\x64\*.manifest" 2>NUL
--- src/libraries/NCRT/lib/types.h
+++ src/libraries/NCRT/lib/types.h
@@ -161,4 +161,24 @@ typedef union _BYTE
 
 #pragma pack(pop)
 
+#ifdef NOCRT_CRT_STUBS
+    #if defined(_M_IX86)
+        int32 _fltused = 0;
+        int32 __cdecl _ftol2(real4 x) { return (int32)x; }
+        real8 __cdecl _ltod3(int64 x) { return (real8)x; }
+        int64 __cdecl _dtol3(real8 x) { return (int64)x; }
+        int64 __cdecl _dtol3_sse(real8 x) { return (int64)x; }
+        int32 __cdecl _ftol2_sse(real4 x) { return (int32)x; }
+        real8 __cdecl _ltod3_sse(int64 x) { return (real8)x; }
+        int32 __cdecl __ftol2_sse(real8 x) { return (int32)x; }
+        int64 __cdecl _allmul(int64 a, int64 b) { return a * b; }
+        int64 __cdecl _allshr(int64 a, uint8 b) { return a >> b; }
+        uint64 __cdecl _aullrem(uint64 a, uint64 b) { return a % b; }
+        uint64 __cdecl _aulldiv(uint64 a, uint64 b) { return a / b; }
+        uint64 __cdecl __aullrem(uint64 a, uint64 b) { return a % b; }
+        uint64 __cdecl __aulldiv(uint64 a, uint64 b) { return a / b; }
+    #elif defined(_M_X64)
+        int32 _fltused = 0;
+    #endif
+#endif
 #endif
